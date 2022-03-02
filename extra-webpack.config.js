cc43232dbeedcbf883ab422c26d9869e0507f1b2
ismail.arilik <ismail.arilik@piworks.net>
2022-03-02 15:40:53 +0300

    Define a function to get Angular version

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -3,6 +3,11 @@ const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').defaul
 module.exports = (config, options) => {
   const singleSpaWebpackConfig = singleSpaAngularWebpack(config, options);
 
+  if (!singleSpaWebpackConfig.externals) {
+    singleSpaAngularWebpack.externals = {};
+  }
+  singleSpaAngularWebpack.externals['getAngularVersion'] = 'getAngularVersion';
+
   // Feel free to modify this webpack config however you'd like to
   return singleSpaWebpackConfig;
 };
--- src/main.single-spa.ts
+++ src/main.single-spa.ts
@@ -8,6 +8,8 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { singleSpaPropsSubject } from './single-spa/single-spa-props';
 
+import { VERSION } from '@angular/core';
+
 if (environment.production) {
   enableProdMode();
 }
@@ -24,3 +26,7 @@ const lifecycles = singleSpaAngular({
 export const bootstrap = lifecycles.bootstrap;
 export const mount = lifecycles.mount;
 export const unmount = lifecycles.unmount;
+
+export const getAngularVersion = () => {
+  return VERSION.full;
+}
