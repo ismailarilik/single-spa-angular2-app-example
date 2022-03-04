36d4a8670edc3b610965115d2373c956506f195e
ismail.arilik <ismail.arilik@piworks.net>
2022-03-04 10:19:54 +0300

    Define webpackConfig.externals every time
    Somehow it wasn't defined when we tried to set it if it is undefined

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -3,9 +3,7 @@ const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').defaul
 module.exports = (config, options) => {
   const singleSpaWebpackConfig = singleSpaAngularWebpack(config, options);
 
-  if (!singleSpaWebpackConfig.externals) {
-    singleSpaAngularWebpack.externals = {};
-  }
+  singleSpaAngularWebpack.externals = {};
   singleSpaAngularWebpack.externals['getAngularVersion'] = 'getAngularVersion';
 
   // Feel free to modify this webpack config however you'd like to
